22b006a412efeaeaca031d0ebc87931f45b01459
Dawid <73873543+Ebsibi@users.noreply.github.com>
2023-06-20 00:57:47 +0200

    Add files via upload

---
+++ ProjektSA/ListaZadan.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_AI
+{
+    internal class Zadanie : ICloneable
+    {
+        public int  IdZadania;             // jednoznaczny indentyfikator zadania
+        public int  CzasWykonaniaZadania;  // czas wykonania zadania wyrażony w sekundach
+        public int? PoprzednieZadanie;     // ID zadania wcześniejszego, takiego, które musi wykonać się przed aktualnym zadaniem
+                                           // de facto zadanie poprzednie i aktualne mogłyby być jednym zadaniem, w sensie sumarycznego czasu
+
+        public Zadanie(int IdZadania, int CzasWykonaniaZadania, int? PoprzednieZadanie = null)
+        {
+            this.IdZadania = IdZadania;
+            this.CzasWykonaniaZadania = CzasWykonaniaZadania;
+            this.PoprzednieZadanie = PoprzednieZadanie;
+        }
+
+        // W zadaniu będę kopiował bazową listę, ponieważ później chcę ułożyć zadania, które w niej występują w sposób losowy
+        // Potrzebuję głębokiej kopii, dlatego implementuję interfejs ICloneable
+        // Muszę zadeklarować w ten sposób, ponieważ implementując po ICLoneable muszę zwracać Object,
+        // natomiast nie mam jawnego rzutowania z Zadanie na Object
+        Object ICloneable.Clone()
+        {
+            return new Zadanie(this.IdZadania, this.CzasWykonaniaZadania, this.PoprzednieZadanie);
+        }
+
+        public Zadanie Clone()
+        {
+            return new Zadanie(this.IdZadania, this.CzasWykonaniaZadania, this.PoprzednieZadanie);
+        }
+    }
+}
---
+++ ProjektSA/Procesor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_AI
+{
+    internal class Procesor
+    {
+        public List<Zadanie>? listaZadań;
+        public int zajętyCzas;
+
+        public Procesor(List<Zadanie>? zadania = null, int zajętyCzas = 0)
+        {
+            this.listaZadań = zadania;
+            this.zajętyCzas = zajętyCzas;
+        }
+    }   
+}
